a30b2c7e694bc3628c01c62c51ed2e70b0833978
Ruben Morales <leormm23@gmail.com>
2024-06-05 12:03:27 -0600

    Usuarios

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, abort
+from flask import Flask, render_template, request, redirect, url_for
 import psycopg2
 
 app = Flask(__name__)
@@ -19,7 +19,7 @@ def not_found_error(error):
 @app.route('/')
 def index():
     return render_template('index.html')
-  
+
 @app.route('/productos')
 def productos():
     try:
@@ -67,6 +67,34 @@ def categorias():
             connection.close()
     
     return render_template('categorias.html', categorias_producto=categorias_producto)
+
+@app.route('/nuevo_empleado', methods=['GET', 'POST'])
+def nuevo_empleado():
+    if request.method == 'POST':
+        nombre = request.form['nombre']
+        apellido = request.form['apellido']
+        edad = request.form['edad']
+        direccion = request.form['direccion']
+        telefono = request.form['telefono']
+        salario = request.form['salario']
+
+        try:
+            connection = get_db_connection()
+            cursor = connection.cursor()
+            cursor.execute(
+                "INSERT INTO empleados (nombre, apellido, edad, direccion, telefono, salario) VALUES (%s, %s, %s, %s, %s, %s)",
+                (nombre, apellido, edad, direccion, telefono, salario)
+            )
+            connection.commit()
+        except Exception as ex:
+            print(ex)
+        finally:
+            if connection:
+                connection.close()
+        
+        return redirect(url_for('empleados'))
+    
+    return render_template('nuevo_empleado.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
